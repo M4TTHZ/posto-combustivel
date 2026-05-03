c963d7d1842103ca9f6d50f66f1f133fb80034b0
Matheus ramalho da silva <matheusramalhoss23@gmail.com>
2026-05-02 23:18:43 -0300

    Feat:
    entity

---
+++ src/main/java/com/matheusramalho/posto_combustivel/infrastructure/entities/BombaDeCombustivel.java
@@ -0,0 +1,26 @@
+package com.matheusramalho.posto_combustivel.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "bombas_de_combustivel")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BombaDeCombustivel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "combustivel_id")
+    private TiposDeCombutivel tiposDeCombutivel;
+
+}
